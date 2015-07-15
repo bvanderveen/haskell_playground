562b95bf77983d951d34afc9750c0d45510e7a55
Benjamin van der Veen <b@bvanderveen.com>
2015-07-14 21:00:51 -0700

    more basic eval

--- tests/HaskellPlayground/PlaygroundSpec.hs
+++ tests/HaskellPlayground/PlaygroundSpec.hs
@@ -9,11 +9,12 @@ spec :: Spec
 spec = do
     describe "tokenizer" $ do
         it "should tokenize" $ do
-            parseLisp "(foo bar (baz 42))" 
+            parseLisp "(foo bar (baz 42) ())" 
             `shouldBe` Right (List [
                 Atom "foo", 
                 Atom "bar", 
-                List [Atom "baz", Number 42]])
+                List [Atom "baz", Number 42],
+                List []])
 
     describe "eval" $ do
         it "should eval numbers" $ do
@@ -21,5 +22,15 @@ spec = do
             
         it "should eval strings" $ do
             parseEval "\"foo\"" `shouldBe` Right (String "foo")
+
+        it "should eval booleans" $ do
+            parseEval "true" `shouldBe` Right (Bool True)
+            parseEval "false" `shouldBe` Right (Bool False)
+
+        it "should eval quotes" $ do
+            parseEval "(quote foo)" `shouldBe` Right (Atom "foo")
+            parseEval "(quote ())" `shouldBe` Right (List [])
+            parseEval "(quote (true false))" `shouldBe` Right (List [Bool True, Bool False])
+
 
 
